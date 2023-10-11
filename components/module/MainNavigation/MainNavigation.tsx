b3c4d24bb738e7891dd55cca9a15fe6dbace50a6
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-10-11 22:26:41 +0330

    add: cpost

--- components/module/MainNavigation/MainNavigation.tsx
+++ components/module/MainNavigation/MainNavigation.tsx
@@ -1,79 +1,84 @@
 import React from 'react'
+import { NextPage } from 'next';
 import HomeIcon from '@mui/icons-material/Home';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import SettingsIcon from '@mui/icons-material/Settings';
 import Image from 'next/image'
 import { Box, Button, Card, Divider, Grid, Typography } from '@mui/material'
 
-const MainNavigation = () => {
-  return (
-    <>
-                           <Card sx={{ textAlign: "center", height: "390px", width: "200px", display: { md: "block", sm: "none", xs: "none" } }}>
-                        <Grid m="15px 0px">
-                            <Image
-                                src="/images/sadra2.jpg"
-                                alt="sadraProfile"
-                                width={100}
-                                height={100}
-                                style={{ borderRadius: "50%" }}
-                            />
-                        </Grid>
-                        <Typography fontWeight="bold" variant="h6" sx={{ margin: "10px 0px" }}>
-                            امیر صدرا نورمحمدی
-                        </Typography>
-                        <Divider />
-                        <Grid m="10px 0px">
-                            <Button>
-                                <Typography fontWeight="bold" variant="h5">
-                                    پنل کاربری
-                                </Typography>
-                            </Button>
-                        </Grid>
-                        <Divider />
-                        <Grid m="10px 0px">
-                            <Button>
-                                <Typography fontWeight="bold" variant="h5">
-                                    ایجاد پست
-                                </Typography>
-                            </Button>
-                        </Grid>
-                        <Divider />
-                        <Grid m="10px 0px">
-                            <Button>
-                                <Typography fontWeight="bold" variant="h5">
-                                    تنظیمات حساب کاربری
-                                </Typography>
-                            </Button>
-                        </Grid>
-                    </Card>
+interface IMainNavigation {
+    selectHandller: (selector: number) => void;
+}
 
-                    <Box 
-                    display={{ md: "none", sm: "block", xs: "block"}}
-                    position="fixed"
-                    bottom="0"
-                    m="10px 0px"
-                    zIndex="10000"
-                    >
-                        <Card sx={{ display: "flex" }}>
-                            <Grid>
-                                <Button>
-                                <HomeIcon />
-                                </Button>
-                            </Grid>
-                            <Grid>
-                            <Button>
-                              <PostAddIcon />
-                              </Button>
-                            </Grid>
-                            <Grid>
-                                <Button>
-                                <SettingsIcon />
-                                </Button>
-                            </Grid>
-                        </Card>
-                    </Box>
-    </>
-  )
+const MainNavigation: NextPage<IMainNavigation> = ({selectHandller}) => {
+    return (
+        <>
+            <Card sx={{ textAlign: "center", height: "390px", width: "200px", display: { md: "block", sm: "none", xs: "none" } }}>
+                <Grid m="15px 0px">
+                    <Image
+                        src="/images/sadra2.jpg"
+                        alt="sadraProfile"
+                        width={100}
+                        height={100}
+                        style={{ borderRadius: "50%" }}
+                    />
+                </Grid>
+                <Typography fontWeight="bold" variant="h6" sx={{ margin: "10px 0px" }}>
+                    امیر صدرا نورمحمدی
+                </Typography>
+                <Divider />
+                <Grid m="10px 0px">
+                    <Button onClick={()=> selectHandller(0)}>
+                        <Typography fontWeight="bold" variant="h5">
+                            پنل کاربری
+                        </Typography>
+                    </Button>
+                </Grid>
+                <Divider />
+                <Grid m="10px 0px">
+                <Button onClick={()=> selectHandller(1)}>
+                        <Typography fontWeight="bold" variant="h5">
+                            ایجاد پست
+                        </Typography>
+                    </Button>
+                </Grid>
+                <Divider />
+                <Grid m="10px 0px">
+                <Button onClick={()=> selectHandller(2)}>
+                        <Typography fontWeight="bold" variant="h5">
+                            تنظیمات حساب کاربری
+                        </Typography>
+                    </Button>
+                </Grid>
+            </Card>
+
+            <Box
+                display={{ md: "none", sm: "block", xs: "block" }}
+                position="fixed"
+                bottom="0"
+                m="10px 0px"
+                zIndex="10000"
+            >
+                <Card sx={{ display: "flex" }}>
+                    <Grid>
+                    <Button onClick={()=> selectHandller(0)}>
+                            <HomeIcon />
+                        </Button>
+                    </Grid>
+                    <Grid>
+                    <Button onClick={()=> selectHandller(1)}>
+                            <PostAddIcon />
+                        </Button>
+                    </Grid>
+                    <Grid>
+                    <Button onClick={()=> selectHandller(2)}>
+                            <SettingsIcon />
+                        </Button>
+                    </Grid>
+                </Card>
+            </Box>
+        </>
+    )
 }
 
 export default MainNavigation;
--- components/template/DashboardPage/DashboardPage.tsx
+++ components/template/DashboardPage/DashboardPage.tsx
@@ -1,21 +1,27 @@
+"use client"
+import { useState } from "react"
 import { NextPage } from 'next';
 import { Grid } from '@mui/material';
 import MainNavigation from '@/components/module/MainNavigation/MainNavigation';
 import Main from '@/components/module/Main/Main';
+import CreatePost from "@/components/module/CreatePost/CreatePost";
 
 
 interface IDashboard {
     data: {
-      userId: number;
-      id: number;
-      title: string;
-      body: string;
+        userId: number;
+        id: number;
+        title: string;
+        body: string;
     }[];
-  }
-  
+}
 
 
-const DashboardPage: NextPage<IDashboard> = ({data}) => {
+
+const DashboardPage: NextPage<IDashboard> = ({ data }) => {
+
+    const [select, setSelect] = useState<number>(0);
+
     return (
         <div style={{ height: "100%", margin: "150px 0px" }}>
             <Grid container justifyContent="center">
@@ -30,7 +36,15 @@ const DashboardPage: NextPage<IDashboard> = ({data}) => {
                         direction: "rtl"
                     }}
                 >
-                    <Main data = {data} />
+                    {
+                        select === 0 ?
+                            <Main data={data} />
+                            :
+                            select === 1 ?
+                                <CreatePost />  
+                                :
+                                ""
+                    }
                 </Grid>
                 <Grid
                     item
@@ -40,7 +54,11 @@ const DashboardPage: NextPage<IDashboard> = ({data}) => {
                     display="flex"
                     justifyContent="center"
                 >
-                        <MainNavigation />
+                    <MainNavigation
+                        selectHandller={(selector) => {
+                            setSelect(selector)
+                        }}
+                    />
                 </Grid>
             </Grid>
         </div>
