da26a1636d4c063f5241ed1ba313bf5ee68e45bc
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-10-15 01:37:54 +0330

    final commit

---
+++ app/sitemap.ts
@@ -0,0 +1,17 @@
+export default async function sitemap() {
+    const staticRoutes = ["", "/blogs", "/contactus"];
+    const res = await import("@/app/api/post/posts/route");
+    const data = await (await res.GET()).json();
+
+    const routes = staticRoutes.map((route:any) => ({
+        url: `https://www.amirsadranormohammadi.com${route}`,
+        lastModified: new Date().toString(),
+    }));
+
+    const blogs = data.data.map((item:any) => ({
+        url: `https://www.amirsadranormohammadi.com/blogs/${item._id}`,
+        lastModified: new Date().toString(),
+    }));
+
+    return [...routes, ...blogs]
+}
