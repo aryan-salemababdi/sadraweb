3e1b57e6d5e966e83f9de2ab2f48faaf5896c117
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-12-22 14:27:23 +0330

    final commit

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,15 +1,15 @@
 import Blogs from '@/components/template/Blogs/Blogs';
+import Posts from "@/models/Posts";
 import React from 'react'
 
 const BlogsPage = async () => {
   
-  const res = await import("@/app/api/post/posts/route");
+  const data = await Posts.find();
 
-  const data = await (await res.GET()).json();
 
   return (
     <>
-      <Blogs data ={data.data} />
+      <Blogs data ={data} />
     </>
   )
 }
