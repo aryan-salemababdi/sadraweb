10c11e0ac833608601a663978a2187724aa22bf8
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2023-12-22 16:29:25 +0330

    final commit

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,21 +1,11 @@
 import Blogs from '@/components/template/Blogs/Blogs';
-import { cookies } from 'next/headers';
 
-
-const BlogsPage = async() => {
-  
+const BlogsPage = async () => {
   const res = await import("../api/post/posts/route");
-
   const data = await (await res.GET()).json();
-
-  const cookieStore = cookies()
-  const theme = cookieStore.get("sadraweb")
-
   return (
     <>
-    {
-       theme ? <Blogs data ={data.data} /> : <Blogs data ={data.data} />
-    }
+       <Blogs data ={data.data} />
     </>
   )
 }
