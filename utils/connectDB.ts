6ae8dd9df294ca386ad2c8cf84cfb30c7dfbac0d
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-03-01 03:07:15 +0330

    some change

--- utils/connectDB.ts
+++ utils/connectDB.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
 async function connectDB() {
+  if (mongoose.connections[0].readyState) return;
 
-    if (mongoose.connections[0].readyState) return;
+  mongoose.set("strictQuery", false);
 
-    mongoose.set("strictQuery",false);
-
-    await mongoose.connect(process.env.MONGO_URI || "" );
-
+  try {
+    await mongoose.connect(process.env.MONGO_URI || "");
     console.log("Connected to DB");
-    
+  } catch (error) {
+    console.error("Error in connecting to DB:", error);
+    throw error;
+  }
 };
 
-
-export default connectDB; 
+export default connectDB;
