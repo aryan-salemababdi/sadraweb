6ae41874203e53e63a489008ea893b67e12020aa
Aryan Salemabadi <54766308+aryan-salemababdi@users.noreply.github.com>
2025-03-01 00:39:40 +0330

    some change

--- app/composition/page.tsx
+++ app/composition/page.tsx
@@ -2,7 +2,7 @@ import Blogs from '@/components/template/Blogs/Blogs';
 
 const BlogsPage = async () => {
   
-  const res = await fetch("https://sadraweb.vercel.app/api/post/posts", {cache: "no-store"})
+  const res = await fetch("https://aryan-salemabadi.vercel.app/api/post/posts", {cache: "no-store"})
   const data = await res.json();
 
   return (
